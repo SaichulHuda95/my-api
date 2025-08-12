0d74d92fcab83c3ea019d4edfb5395babaf84ef0
Saichul Huda <saichulhuda1995@gmail.com>
2025-08-12 14:38:54 +0700

    add composer.json

--- modules/qrcode/create.php
+++ modules/qrcode/create.php
@@ -17,47 +17,47 @@ Flight::route('POST /qrcode/create', function () {
     QRcode::png($url, null, QR_ECLEVEL_H, 10);
     $qrData = ob_get_clean();
 
-    // $QR = imagecreatefromstring($qrData);
+    $QR = imagecreatefromstring($qrData);
 
-    // // Cek logo
-    // if (!empty($_FILES['logo']) && $_FILES['logo']['error'] === UPLOAD_ERR_OK) {
-    //     $logoTmpPath = $_FILES['logo']['tmp_name'];
-    //     $logoImg = @imagecreatefromstring(file_get_contents($logoTmpPath));
+    // Cek logo
+    if (!empty($_FILES['logo']) && $_FILES['logo']['error'] === UPLOAD_ERR_OK) {
+        $logoTmpPath = $_FILES['logo']['tmp_name'];
+        $logoImg = @imagecreatefromstring(file_get_contents($logoTmpPath));
 
-    //     if ($logoImg) {
-    //         $QR_width = imagesx($QR);
-    //         $QR_height = imagesy($QR);
+        if ($logoImg) {
+            $QR_width = imagesx($QR);
+            $QR_height = imagesy($QR);
 
-    //         $logo_width = imagesx($logoImg);
-    //         $logo_height = imagesy($logoImg);
+            $logo_width = imagesx($logoImg);
+            $logo_height = imagesy($logoImg);
 
-    //         $logo_qr_width = $QR_width / 5;
-    //         $scale = $logo_width / $logo_qr_width;
-    //         $logo_qr_height = $logo_height / $scale;
+            $logo_qr_width = $QR_width / 5;
+            $scale = $logo_width / $logo_qr_width;
+            $logo_qr_height = $logo_height / $scale;
 
-    //         $from_width = ($QR_width - $logo_qr_width) / 2;
+            $from_width = ($QR_width - $logo_qr_width) / 2;
 
-    //         imagecopyresampled(
-    //             $QR,
-    //             $logoImg,
-    //             (int) $from_width,
-    //             (int) $from_width,
-    //             0,
-    //             0,
-    //             (int) $logo_qr_width,
-    //             (int) $logo_qr_height,
-    //             (int) $logo_width,
-    //             (int) $logo_height
-    //         );
-    //     }
-    // }
+            imagecopyresampled(
+                $QR,
+                $logoImg,
+                (int) $from_width,
+                (int) $from_width,
+                0,
+                0,
+                (int) $logo_qr_width,
+                (int) $logo_qr_height,
+                (int) $logo_width,
+                (int) $logo_height
+            );
+        }
+    }
 
-    // // Encode gambar ke base64
-    // ob_start();
-    // imagepng($QR);
-    // $imageData = ob_get_clean();
+    // Encode gambar ke base64
+    ob_start();
+    imagepng($QR);
+    $imageData = ob_get_clean();
 
-    $base64Image = 'data:image/png;base64,' . base64_encode($qrData);
+    $base64Image = 'data:image/png;base64,' . base64_encode($imageData);
 
     Flight::json([
         'code' => 200,
